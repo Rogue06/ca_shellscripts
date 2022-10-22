c83e8f02a3131cd1757eb223c5df5d384aec159e
Rogue06 <roguenftproject@gmail.com>
2022-10-22 12:09:27 +0200

    Add last exercice to LearnShell

--- Shell_Functions.sh
+++ Shell_Functions.sh
@@ -1,13 +1,26 @@
-#! /bin/bash
+#!/bin/bash
+# enter your function code here
+#Dans un script bash, $1 représente le 1er paramètre passée à la fonction..
+#donc  en lançant ENGLISH_CALC 3 plus 5
+#$1 reçoit 3; $2 reçoit plus; $3 reçoit 5
 
-calcul1=8
-calcul2=4
-calcul3=24
+
+a=$1
+b=$3
+signn=$2
 
 function ENGLISH_CALC {
-    echo $1 $2 $3
-}
 
-echo "3 + 5 = $calcul1"
-echo "5 - 1 = $calcul2"
-echo "4 * 6 = $calcul3"
+if [ $signn == "plus" ]; then
+	echo "$a + $b = $(($a+$b))"
+elif [ $signn == "minus" ]; then
+	echo "$a - $b = $(($a-$b))"
+elif [ $signn == "times" ]; then
+	echo "$a * $b = $(($a*$b))"
+fi
+{
+
+# testing code
+ENGLISH_CALC 3 plus 5
+ENGLISH_CALC 5 minus 1
+ENGLISH_CALC 4 times 6
